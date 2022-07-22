4e4a014a83cdd1ae2f899b677f68d1c692544cc7
Ekaterina Chepova <kate.chepova@yandex.ru>
2022-07-22 18:32:34 +0300

    last version

--- hw1.js
+++ hw1.js
@@ -25,13 +25,31 @@ let item_7 = String(item_6)
 let item_7_type = typeof(item_7)
 console.log ("item_7 == " + item_7, "item_7_type == " + item_7_type)
 
-let age_1 = 18, age_2 = 18, age_3 = 60
-if(age_1 < age_2){
-    console.log ("You don’t have access cause your age is " + age_1 + ". It’s less then " + age_2)
-} else if(age_1 >= age_2, age_1 < age_3){
-    console.log ("Welcome!")
-}else if(age_1 > age_3){
-    console.log ("Keep calm and look Culture channel")
-}else {
-    console.log ("Technical work")
+// let age_1 = 18, age_2 = 18, age_3 = 60
+// if(age_1 < age_2){
+//     console.log ("You don’t have access cause your age is " + age_1 + ". It’s less then " + age_2)
+// } else if(age_1 >= age_2, age_1 < age_3){
+//     console.log ("Welcome!")
+// }else if(age_1 > age_3){
+//     console.log ("Keep calm and look Culture channel")
+// }else {
+//     console.log ("Technical work")
+// }
+
+const age2 = 18
+const age3 = 60
+const checkAge = function(age){
+    if(age < age2){
+        console.log ("You don’t have access cause your age is " + age + ". It’s less then " + age2)
+    } else if(age >= age2, age < age3){
+        console.log ("Welcome!")
+    }else if(age > age3){
+        console.log ("Keep calm and look Culture channel")
+    }else {
+        console.log ("Technical work")
+    }
 }
+checkAge(17)
+checkAge(19)
+checkAge(60)
+checkAge(62)
